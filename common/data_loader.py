029e61fb94e6e117fc8c5c3ccde57a2bcdcf9792
ArnauNaval <arnaunaval@gmail.com>
2022-09-25 17:07:20 +0200

    creation of train and test datasets

--- common/data_loader.py
+++ common/data_loader.py
@@ -10,7 +10,7 @@ class DataLoader:
     def __init__(self):
         self.paths = [
             (
-                "data/archive.zip",
+                "data/real_vs_fake.zip",
                 "https://drive.google.com/file/d/1xj7RbvX_rGvqBk2mFXSLVSCubCujRRh1/view?usp=sharing",
             )
         ]
@@ -23,24 +23,49 @@ class DataLoader:
             if not os.path.exists(path):
                 download_from_drive(path, url)  # assume all zips will be in drive
 
-    def load_image_from_path(self, path, channels=3):
-        image = tf.io.read_file(path)
-        image = tf.image.decode_jpeg(image, channels=channels)
-
-        image = tf.image.convert_image_dtype(image, tf.float32)
-        return image
-
     def check_if_datasets_are_unzipped(self):
         data_path = pathlib.Path("data")
 
         for zip_file in data_path.glob("*.zip"):
             dir_ = os.path.splitext(zip_file)[0]
 
-            if not os.path.exists(
-                dir_
-            ):  # assume name of the dir is the same as name of the zip
+            if not os.path.exists(dir_):
+                # assume name of the dir is the same as name of the zip
                 with ZipFile(zip_file, "r") as zipObj:
                     zipObj.extractall(data_path)
+
+    def load_image_from_path(self, path, channels=3):
+        image = tf.io.read_file(path)
+        image = tf.image.decode_jpeg(image, channels=channels)
+        image = tf.image.convert_image_dtype(image, tf.float32)
+
+        return image
+
+    # Normalizing the images to [-1, 1]
+    def normalize(self, image):
+        image = (image / 127.5) - 1
+        return image
+
+    def image_augmentation(self, image):
+        pass
+
+    def image_resizing(self, image):
+        pass
+
+    def load_image_train(self, image_file):
+        input_image = self.load_image_from_path(image_file)
+        # input_image = self.image_augmentation(input_image)
+        # input_image = self.image_resizing(input_image)
+        input_image = self.normalize(input_image)
+
+        return input_image
+
+    def load_image_test(self, image_file):
+        input_image = self.load_image_from_path(image_file)
+        # input_image = self.image_resizing(input_image)
+        input_image = self.normalize(input_image)
+
+        return input_image
 
 
 # TODO:
--- main.py
+++ main.py
@@ -1,3 +1,14 @@
 from common.data_loader import DataLoader
+import tensorflow as tf
+
+BUFFER_SIZE = 10
+BATCH_SIZE = 10
+PATH = "data/real_vs_fake/real-vs-fake/train/fake"
 
 DL = DataLoader()
+train_dataset = tf.data.Dataset.list_files(str(PATH + "/*.jpg"))
+train_dataset = train_dataset.map(
+    DL.load_image_train, num_parallel_calls=tf.data.AUTOTUNE
+)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
