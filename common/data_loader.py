d8815e731116c03d83dff7d731d217088f18a49c
ArnauNaval <arnaunaval@gmail.com>
2022-09-27 19:10:55 +0200

    wip labels

--- common/data_loader.py
+++ common/data_loader.py
@@ -1,8 +1,11 @@
+from email.mime import image
 import tensorflow as tf
 import os
 import pathlib
-from zipfile import ZipFile
+import pandas as pd
+import numpy as np
 
+from zipfile import ZipFile
 from common.GoogleDrive import download_from_drive
 
 
@@ -17,6 +20,7 @@ class DataLoader:
 
         self.check_if_datasets_are_downloaded()
         self.check_if_datasets_are_unzipped()
+        self.labels = None
 
     def check_if_datasets_are_downloaded(self):
         for path, url in self.paths:
@@ -33,6 +37,9 @@ class DataLoader:
                 # assume name of the dir is the same as name of the zip
                 with ZipFile(zip_file, "r") as zipObj:
                     zipObj.extractall(data_path)
+
+    def read_labels(self, labels_path):
+        return pd.read_csv(labels_path)
 
     def load_image_from_path(self, path, channels=3):
         image = tf.io.read_file(path)
@@ -53,22 +60,17 @@ class DataLoader:
         pass
 
     def load_image_train(self, image_file):
+        self.labels = self.read_labels("data/train.csv")
         input_image = self.load_image_from_path(image_file)
         # input_image = self.image_augmentation(input_image)
         # input_image = self.image_resizing(input_image)
         input_image = self.normalize(input_image)
-
-        return input_image
+        return input_image, self.labels.loc[self.labels["path"] == image_file].label
 
     def load_image_test(self, image_file):
+        print(image_file[0])
+        self.labels = self.read_labels("data/test.csv")
         input_image = self.load_image_from_path(image_file)
         # input_image = self.image_resizing(input_image)
         input_image = self.normalize(input_image)
-
-        return input_image
-
-
-# TODO:
-# 1. chech if datasets are downloaded DONE
-# 2. check if datasets are unzipped DONE
-# 3. transform dataset to common structure
+        return input_image, self.labels.loc[self.labels["path"] == image_file].label
--- main.py
+++ main.py
@@ -1,15 +1,25 @@
 from common.data_loader import DataLoader
 import tensorflow as tf
 
-BUFFER_SIZE = 10
-BATCH_SIZE = 10
-PATH = "data/real-vs-fake/train"
+BUFFER_SIZE = 1
+BATCH_SIZE = 1
+PATH = "data/real-vs-fake/test"
+tf.executing_eagerly()
 
 DL = DataLoader()
 train_dataset = tf.data.Dataset.list_files(str(PATH + "/*.jpg"))
-train_dataset = train_dataset.map(
-    DL.load_image_train, num_parallel_calls=tf.data.AUTOTUNE
-)
+print(train_dataset)
+# train_dataset = train_dataset.map(
+#    DL.load_image_test, num_parallel_calls=tf.data.AUTOTUNE
+# )
 
-train_dataset = train_dataset.shuffle(BUFFER_SIZE)
-train_dataset = train_dataset.batch(BATCH_SIZE)
+# train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+# train_dataset = train_dataset.batch(BATCH_SIZE)
+"""
+for i in train_dataset:
+    print(len(i))
+    print(type(i[0]))
+    print(type(i[1]))
+    print(i[1].numpy())
+    break
+"""
