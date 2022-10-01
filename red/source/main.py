ec3df572f995003a607a7c83579844b396ffb385
mathi <mathias_a_sandnes@hotmail.com>
2022-10-01 11:40:30 +0200

    Cleaned directory structure

--- red/source/main.py
+++ red/source/main.py
@@ -1,5 +1,9 @@
+from common.data_loader import DataLoader
 from red.source.utils import get_config
 
 if __name__ == '__main__':
     config = get_config()
-    print(config)
+
+    DL = DataLoader()
+    train_dataset = DL.get_data("train")
+    test_dataset = DL.get_data("test")
