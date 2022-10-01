b76c6c2d3bcb8c8a5cc4a9a30fc158b2d42b05e6
mathi <mathias_a_sandnes@hotmail.com>
2022-10-01 12:55:39 +0200

    Prepped for development

--- blue/source/main.py
+++ blue/source/main.py
@@ -1,5 +1,33 @@
-from blue.source.utils import get_config
+import tensorflow as tf
+from keras.callbacks import EarlyStopping
+import os
 
-if __name__ == '__main__':
-    config = get_config()
-    print(config)
+from blue.source.akselnet import get_akselnet
+from common.data_loader import DataLoader
+from common.utils import get_config
+
+config = get_config()
+
+
+def get_model():
+    match config['model_name']:
+        case 'akselnet':
+            return get_akselnet()
+
+
+if __name__ == "__main__":
+    DL = DataLoader()
+    train_ds = DL.get_data("train")
+    test_ds = DL.get_data("test")
+
+    model = get_model()
+
+    early_stopping_callback = EarlyStopping(monitor='val_accuracy', patience=5)
+    # mm-dd-hh-mm-config['model_name']
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=os.path.join(config['tensorboard']['log_dir'], 'akselnet'))
+
+    model.fit(x=train_ds,
+              batch_size=config['batch_size'],
+              validation_data=test_ds,
+              epochs=5,
+              callbacks=[early_stopping_callback])
