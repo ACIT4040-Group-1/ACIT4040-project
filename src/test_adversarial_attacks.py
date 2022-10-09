d7dbb454214064153f8574ef34757c57127299cb
guttulsrud <guttulsrud@outlook.com>
2022-10-09 23:07:53 +0200

    Added new red classifier model

--- src/test_adversarial_attacks.py
+++ src/test_adversarial_attacks.py
@@ -3,7 +3,8 @@ from tensorflow import keras
 from keras.preprocessing.image import ImageDataGenerator
 
 if __name__ == '__main__':
-    model = keras.models.load_model('red/classifier/trained_models/dense_net_trained.h5')
+    model = keras.models.load_model('red/classifier/trained_models/red_classifier.h5')
+    # model = keras.models.load_model('red/classifier/trained_models/dense_net_trained.h5')
 
     image_gen = ImageDataGenerator(rescale=1. / 255.)
     test_images = image_gen.flow_from_directory('red/test_data', target_size=(224, 224), batch_size=1,
