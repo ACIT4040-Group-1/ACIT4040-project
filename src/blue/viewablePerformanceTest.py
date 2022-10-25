d23af1187e4ed2eb8bf4fee638a279ec1850cbb1
akseljohan <42238685+akseljohan@users.noreply.github.com>
2022-10-25 13:54:44 +0200

    added copy config and changed log/model structure

---
+++ src/blue/viewablePerformanceTest.py
@@ -0,0 +1,9 @@
+import keras
+from src.common.utils import plot_predictions
+from src.common.DataLoader import DataLoader
+
+DL = DataLoader()
+data = DL.get_data('valid')
+model = keras.models.load_model('C:/Users/aksel/OneDrive/ACIT-Robotikk/ACIT4040 - Project/trained_models/resNet151_detector.h5')
+# result = model.predict(data)
+plot_predictions(data, model, batch=1)
